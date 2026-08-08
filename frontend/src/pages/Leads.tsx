56aee64f76b52030176e6673d2c1b8644b496030
Ronak Singh <87173646+ronaksorout09dec@users.noreply.github.com>
2026-08-08 23:26:20 +0530

    Delete frontend directory

--- frontend/src/pages/Leads.tsx
+++
@@ -1,9 +0,0 @@
-import LeadList from '../components/LeadList';
-
-export default function Leads() {
-  return (
-    <div style={{ padding: '2rem 1.5rem', background: 'var(--bg)', minHeight: 'calc(100vh - 70px)' }}>
-      <LeadList />
-    </div>
-  );
-}
